e938cb8eb64e04cc71697d7f1a6e6e07a1848aba
kiranbinoy <kiranbinoy257@gmail.com>
2024-04-24 11:35:45 +0530

    screen

--- js/smal lcalculation.js
+++ js/smal lcalculation.js
@@ -1,26 +1,25 @@
 function Addiction(){
     let Number1=parseInt(document.getElementById("Number1").value)
-    
     let Number2=parseInt(document.getElementById("Number2").value)
-    console.log(Number1+Number2);
+    document.getElementById("plus").innerHTML=(Number1+Number2)
 }
 function substraction (){
     let Number1=parseInt(document.getElementById("Number1").value)
     let Number2=parseInt(document.getElementById("Number2").value)
-    console.log(Number1-Number2);
+    document.getElementById("minus").innerHTML=(Number1-Number2)
 }
 function multiplication (){
     let Number1=parseInt(document.getElementById("Number1").value)
     let Number2=parseInt(document.getElementById("Number2").value)
-    console.log(Number1*Number2);
+    document.getElementById("into").innerHTML=(Number1*Number2)
 }
 function division (){
     let Number1=parseInt(document.getElementById("Number1").value)
     let Number2=parseInt(document.getElementById("Number2").value)
-    console.log(Number1/Number2);
+    document.getElementById("divi").innerHTML=(Number1/Number2)
 }
 function modulos (){
     let Number1=parseInt(document.getElementById("Number1").value)
     let Number2=parseInt(document.getElementById("Number2").value)
-    console.log(Number1%Number2);
+    document.getElementById("mode").innerHTML=(Number1%Number2)
 }
