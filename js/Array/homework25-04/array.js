7a5ceda830d11a7cb2e4e705eb1fffc37b116f61
kiranbinoy <kiranbinoy257@gmail.com>
2024-04-24 23:39:16 +0530

    homework

---
+++ js/Array/homework25-04/array.js
@@ -0,0 +1,8 @@
+let a=[]
+function data(){
+    let email=document.getElementById("email").value
+    a.push  (name=document.getElementById("name").value)
+    a.push(email=document.getElementById("email").value)
+    document.getElementById("output").innerHTML=(a)
+    
+}
